579883f55d6081305a4c847430917236db2ffd01
AnkhEl <t.alex.khumalo@gmail.com>
2020-08-14 11:14:46 +0200

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ConnectingToDb
+{
+    class Employee
+    {
+        
+        [Key]
+        public string Employeeguid { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public DateTime Birthday { get; set; }
+        public bool Active { get; set; }
+        public string Street { get; set; }
+        public string Postal { get; set; }
+        public string Province { get; set; }
+        public string City { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public string Package { get; set; }
+        public string Vendor { get; set; }
+        public int Phone { get; set; }
+        public decimal PackageCost { get; set; }
+        public DateTime ContractStart { get; set; }
+        public DateTime ContractEnd { get; set; }
+
+
+        ///package vendor phone   package cost    contract start  contract end
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConnectingToDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Nebula;Integrated Security=True;
+                                Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;
+                                MultiSubnetFailover=False";
+            
+
+            //Create Connection and prepare sql query
+            SqlConnection conn = new SqlConnection(connString);
+            string sql = @"UPDATE Employee 
+                            set email = CONCAT(LEFT(email,CHARINDEX('@',email)),'company.co.za') 
+                             WHERE email IS NOT NULL";
+            
+
+            try
+            {
+                //open connection
+                conn.Open();
+                Console.WriteLine("Database connection state: {0}",conn.State);
+
+                //prepare to query the database
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //execute the update command 
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("Emails updated successfully!");
+
+            }
+            catch(SqlException e)
+            {
+                Console.WriteLine("There was an error while communicating with the database, Error: {0}", e.Message);
+            }
+            finally{
+                
+                //Close DB connection
+                conn.Close();
+                Console.WriteLine("Database connection state: {0}",conn.State);
+            }
+           
+        }
+    }
+}
